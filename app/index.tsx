ca58bb004be467de63525569b4c81aa77046f46b
RenanBrand <renanbrandmelo@gmail.com>
2025-06-08 17:10:54 -0300

    estilos

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,73 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image source={require('../assets/images/foco.png')} />
+      <View style={styles.actions}>
+        <Text style={styles.timer}>25:00</Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>
+            Começar
+          </Text>
+        </Pressable>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto fictício e sem fins comerciais.
+        </Text>
+        <Text style={styles.footerText}>
+          Desenvolvido por Alura.
+        </Text>
+      </View>
     </View>
+
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#021123',
+    gap: 40,
+  },
+  text: {
+    color: "#FFF",
+  },
+  actions: {
+    paddingVertical: 24,
+    paddingHorizontal: 24,
+    backgroundColor: "#14448080",
+    width: "80%",
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: "#144480",
+    gap: 32
+  },
+  timer: {
+    fontSize: 54,
+    color: '#FFF',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#021123',
+    fontSize: 18
+  },
+  button: {
+    backgroundColor: "#B872FF",
+    borderRadius: 32,
+    padding: 10,
+  },
+  footer: {
+    width: '80%',
+  },
+  footerText: {
+    textAlign: 'center',
+    color: '#98A0A8',
+    fontSize: 12.5
+  }
+});
